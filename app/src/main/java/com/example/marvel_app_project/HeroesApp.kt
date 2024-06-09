5abb0e96c92d6c1d03abfdf86d960ba78f61e5b3
Roman194 <RAZ3113@yandex.ru>
2024-06-09 13:22:31 +0600

    HeroesNotification was added to make custom notification with usage of data from the FCM server

--- app/src/main/java/com/example/marvel_app_project/HeroesApp.kt
+++ app/src/main/java/com/example/marvel_app_project/HeroesApp.kt
@@ -1,8 +1,36 @@
 package com.example.marvel_app_project
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import com.example.marvel_app_project.notification.HeroesNotification
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class HeroesApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                HeroesNotification.HEROES_CHANNEl_ID,
+                "counter",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            channel.description = "Used for informing you about hero"
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel)
+
+        }
+    }
 }
